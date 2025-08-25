35795d62858e8a23a43096dc65436657b22f9366
DebbieKloos <debbie@formika.nl>
2025-08-25 07:10:16 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import React, { useState } from "react";
 import InputWizard from "./components/InputWizard";
 import ChecklistOutput from "./components/ChecklistOutput";
-import { ChecklistItem } from "./lib/generateChecklist";
+import { generateChecklist, ChecklistItem } from "./lib/generateChecklist";
 import "./App.css";
+
+export interface FormData {
+  functie: string;
+  startdatum: string;
+  taken: string[];
+  notities: string;
+}
 
 function App() {
   const [checklist, setChecklist] = useState<ChecklistItem[]>([]);
+
+  const handleSubmit = (data: FormData) => {
+    const { taken, startdatum, notities } = data;
+    const result = generateChecklist(taken, startdatum, notities);
+    setChecklist(result);
+  };
 
   return (
     <div className="max-w-2xl mx-auto p-6">
@@ -13,10 +26,7 @@ function App() {
         Slim Inwerklijstje
       </h1>
 
-      {/* Input gedeelte */}
-      <InputWizard onChecklistGenerated={setChecklist} />
-
-      {/* Output gedeelte */}
+      <InputWizard onSubmit={handleSubmit} />
       <ChecklistOutput checklist={checklist} />
     </div>
   );
